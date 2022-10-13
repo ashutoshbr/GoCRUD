3484aa3d3f2b50c099dc810390d6e2e2f8886a32
ashutoshbr <ashutoshbr@proton.me>
2022-10-13 13:21:42 +0545

    Add environment variables 🔐

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ashutoshbr/GoCRUD
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type SimpleResponse struct {
@@ -21,9 +25,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	log.Println("Listening on port 8000")
-	err := http.ListenAndServe(":8000", mux)
+	// check if env variables are loaded
+	fmt.Println(os.Getenv("MONGOUSER"))
+	err = http.ListenAndServe(":8000", mux)
 	log.Fatal(err)
 }
