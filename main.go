2bd16da99bb6122dfb5edb00c9701d32bf836fcc
ashutoshbr <ashutoshbr@proton.me>
2022-10-13 12:03:01 +0545

    Make server 🖥️

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Namaste"))
+}
 
 func main() {
-	fmt.Println("Namaste!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Println("Listening on port 8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
 }
