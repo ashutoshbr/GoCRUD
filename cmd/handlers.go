1403e9f34b40a3a34729ca34d055baa3eef7ea7f
ashutoshbr <ashutoshbr@proton.me>
2022-10-16 09:48:08 +0545

    Heavy refactor 🥴

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,46 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/ashutoshbr/GoCRUD/database"
-	"github.com/ashutoshbr/GoCRUD/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/ashutoshbr/GoCRUD/database"
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
 	// db connection
-	client, ctx, cancel := database.Main()
-	defer database.Close(client, ctx, cancel)
-	collection := client.Database(os.Getenv("DBNAME")).Collection(os.Getenv("COLLNAME"))
+	client := database.Connect()
+	coll := client.Database(os.Getenv("DBNAME")).Collection(os.Getenv("COLLNAME"))
+	defer client.Disconnect(context.TODO())
 
+	// specify GET JSON format
 	filter := bson.D{}
 	projection := bson.D{{"name", 1}, {"age", 1}, {"_id", 0}}
 	opts := options.Find().SetProjection(projection)
 
-	cursor, err := collection.Find(ctx, filter, opts)
+	cursor, err := coll.Find(context.TODO(), filter, opts)
 	if err != nil {
 		panic(err)
 	}
+
 	var results []bson.D
-	err = cursor.All(ctx, &results)
+	err = cursor.All(context.TODO(), &results)
 	for _, result := range results {
 		fmt.Println(result)
 	}
 }
 
-func Create(w http.ResponseWriter, r *http.Request) {
-	// db connection
-	client, ctx, cancel := database.Main()
-	defer database.Close(client, ctx, cancel)
-	collection := client.Database(os.Getenv("DBNAME")).Collection(os.Getenv("COLLNAME"))
+// func Create(w http.ResponseWriter, r *http.Request) {
+// 	// db connection
+// 	// client, ctx, cancel := database.Main()
+// 	var client, ctx, cancel, err = database.Connect(URI)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer database.Close(client, ctx, cancel)
+// 	collection := client.Database(os.Getenv("DBNAME")).Collection(os.Getenv("COLLNAME"))
 
-	person1 := models.Person{"Foo Bar", 20}
-	_, err := collection.InsertOne(ctx, person1)
-	if err != nil {
-		panic(err)
-	}
-}
+// 	person1 := models.Person{"Foo Bar", 20}
+// 	_, err = collection.InsertOne(ctx, person1)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
